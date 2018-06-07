e1849587c568f5bdf0f734df84cf6647969a4782
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-06-07 17:12:08 +1200

    end of thurs week 2

--- routes.js
+++ routes.js
@@ -38,6 +38,10 @@ function getData (callback) {
   const filePath = path.join(__dirname, 'data.json')
   fs.readFile(filePath, 'utf8', callback)
 }
+function puppyDataWrite (puppies, callback) {
+  const puppyPath = path.join(__dirname, 'data.json')
+  fs.writeFile(puppyPath, puppies, 'utf8', callback)
+}
 
 router.get('/puppies/edit/:id', (req, res) => {
   const id = Number(req.params.id)
@@ -51,32 +55,51 @@ router.get('/puppies/edit/:id', (req, res) => {
     }
   })
 })
+// router.post('/puppies/edit/:id', (req, res) => {
+// const id = Number(req.params.id)
+// const puppyInfo = {
+//   id: id,
+//   name: req.body.name,
+//   owner: req.body.owner,
+//   breed: req.body.breed
+// }
 router.post('/puppies/edit/:id', (req, res) => {
-  const name = req.body.name
-  console.log(name)
-  res.render('./puppies/index', pupData)
   const id = Number(req.params.id)
-  // const puppyInfo = {
-  //   id: id,
-  //   name: req.body.name,
-  //   owner: req.body.owner,
-  //   breed: req.body.breed
-  // }
+  const name = req.body.name
+  const breed = req.body.breed
+  const owner = req.body.owner
+  getData(pupFileData)
+  function pupFileData (err, data) {
+    if (err) {
+      res.send('error recieved')
+    } else {
+      const id = Number(req.params.id)
+      const puppies = JSON.parse(data)
+      console.log(puppies.puppies[1].name)
+      puppies.puppies[id - 1].name = name
+      puppies.puppies[id - 1].breed = breed
+      puppies.puppies[id - 1].owner = owner
+      const newPuppies = JSON.stringify(puppies, null, 3)
+      puppyDataWrite(newPuppies, (err, data) => {
+        if (err) { res.send('error sending data').status(500) } else {
+          res.redirect('/puppies/' + id)
+        }
+      })
+    }
+  }
 
   // getData((err, data) => {
   //   if (err) {
   //     res.status(500).send('error')
   //   } else {
   //     const pupData = JSON.parse(data)
-  //     pupData.puppies[id - 1].name = puppyInfo.name
-  //     pupData.puppies[id - 1].owner = puppyInfo.owner
-  //     pupData.puppies[id - 1].breed = puppyInfo.breed
+  //     pupData.puppies[id - 1] = puppyInfo
 
   //     fs.writeFile('./data.json', JSON.stringify(pupData), (err) => {
   //       if (err) {
   //         res.status(500).send('error')
   //       } else {
-  //         res.redirect('/puppies/:id')
+  //         res.redirect('/puppies/' + id)
   //       }
   //     })
   //   }
